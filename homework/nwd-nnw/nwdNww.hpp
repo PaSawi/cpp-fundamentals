a9d0e9a11dd42e7d04567620ddb67c5f0bc737fc
PaSawi <sawicki.patryk@gmail.com>
2022-12-23 11:42:11 +0100

    clang format

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,48 +1,37 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    if(lhs < 0)
-    {
-        lhs = -lhs;   
+int NWD(int lhs, int rhs)
+{
+    if (lhs < 0) {
+        lhs = -lhs;
     }
-    if(rhs < 0)
-    {
-        rhs = -rhs;   
+    if (rhs < 0) {
+        rhs = -rhs;
     }
 
-    if(lhs > rhs) 
-    {
+    if (lhs > rhs) {
         return NWD(rhs, lhs);
-    }
-    else
-    {
-        if(lhs == 0 && rhs == 0) 
-        {
+    } else {
+        if (lhs == 0 && rhs == 0) {
             return 0;
-        }
-        else if(lhs == 0)
-        {
+        } else if (lhs == 0) {
             return rhs;
-        }
-        else{
+        } else {
             return NWD(rhs % lhs, lhs);
         }
     }
 }
-int NWW(int lhs, int rhs) {
-    if(lhs < 0)
-    {
-        lhs = -lhs;   
+int NWW(int lhs, int rhs)
+{
+    if (lhs < 0) {
+        lhs = -lhs;
     }
-    if(rhs < 0)
-    {
-        rhs = -rhs;   
+    if (rhs < 0) {
+        rhs = -rhs;
     }
-    if(lhs == 0 && rhs == 0)
-    {
-            return 0;
-    }
-    else{
+    if (lhs == 0 && rhs == 0) {
+        return 0;
+    } else {
         return (lhs * rhs) / NWD(lhs, rhs);
     }
 }
